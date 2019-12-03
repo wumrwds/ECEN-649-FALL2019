0e0fcaf24acad830b9d84c45f4e9f10f44fc4f5d
Minreng Wu <wumrwds@gmail.com>
2019-12-03 02:40:51 -0600

    feat: added three vertical feature

--- project/haar_features.py
+++ project/haar_features.py
@@ -57,6 +57,21 @@ class Feature3h(Feature):
                        -1, 1, 1, -1]
 
 
+class Feature3v(Feature):
+    def __init__(self, x: int, y: int, width: int, height: int):
+        super().__init__(x, y, width, height)
+        th = height // 3
+        self.coords_x = [x, x + width, x, x + width,
+                         x, x + width, x, x + width,
+                         x, x + width, x, x + width]
+        self.coords_y = [y, y, y + th, y + th,
+                         y + th, y + th, y + 2 * th, y + 2 * th,
+                         y + 2 * th, y + 2 * th, y + height, y + height]
+        self.coeffs = [-1, 1, 1, -1,
+                       1, -1, -1, 1,
+                       -1, 1, 1, -1]
+
+
 class Feature4(Feature):
     def __init__(self, x: int, y: int, width: int, height: int):
         super().__init__(x, y, width, height)
